8f8767a5fc74aaf4f2a1bf07f5a8593bb8b9cbe0
Andrey Zavrichko <andrey@zavrichko.dev>
2022-04-16 17:42:30 +0300

    - создание проекта

--- src/test/java/ru/zavrichko/TestBase.java
+++ src/test/java/ru/zavrichko/TestBase.java
@@ -17,11 +17,8 @@ public class TestBase {
         String url = System.getProperty("url");
         String remoteUrl = url;
         Configuration.remote = remoteUrl;
-     //   Configuration.remote = "http://149.154.70.38:4444/wd/hub";
         String browser = System.getProperty("browser");
         Configuration.browser = browser;
-      // String browserVersion = System.getProperty("version");
-       // Configuration.browser = browserVersion;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
