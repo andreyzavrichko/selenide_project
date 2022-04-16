f400594e6c3afb3bab3024fd4dbe7f1088e50f57
Andrey Zavrichko <andrey@zavrichko.dev>
2022-04-16 15:58:15 +0300

    - создание проекта

--- src/test/java/ru/zavrichko/TestBase.java
+++ src/test/java/ru/zavrichko/TestBase.java
@@ -6,6 +6,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.zavrichko.helpers.AllureAttachments;
 
 public class TestBase {
@@ -14,6 +15,17 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "http://pizzeria.skillbox.cc/";
         Configuration.startMaximized = true;
+        String url = System.getProperty("url");
+        String remoteUrl = "https://" + url;
+        Configuration.remote = remoteUrl;
+        String browser = System.getProperty("browser");
+        Configuration.browser = browser;
+        String browserVersion = System.getProperty("browserVersion");
+        Configuration.browser = browserVersion;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
